97065a4ca32ff2e834429f6f68e5abcc0187eeca
joaquin bustos <joaquinnicolasbustos@hotmail.com>
2025-12-02 13:13:43 -0300

    2/12/25

---
+++ Ejercicios/1.js
@@ -0,0 +1,16 @@
+/*
+Ejercicio 31:
+
+Crea un semáforo con tres Luces (rojo, amarillo y verde)
+Haz los circulos del semáforo con HTML y CSS.
+
+Cuando un peatón quiere cruzar la calle debe pulsar un botón.
+Crea un botón para cambiar el color de las luces del semáforo
+secuencialmente (de rojo a verde, pasando por amarillo).
+
+Entonces,
+El semáforo siempre empieza en rojo.
+Cuando haces click en el botón, pasará a amarillo.
+Al hacer click otra vez, pasa a verde, y así todo el rato.
+
+*/
